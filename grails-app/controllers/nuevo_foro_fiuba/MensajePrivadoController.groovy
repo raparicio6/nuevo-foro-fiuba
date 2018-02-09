46f30f07633b524b9629096bb08d306291ffb9e3
Germán Rotili <German-Rotili@users.noreply.github.com>
2018-02-09 00:48:53 -0300

    Controller Mensaje

--- grails-app/controllers/nuevo_foro_fiuba/MensajePrivadoController.groovy
+++ grails-app/controllers/nuevo_foro_fiuba/MensajePrivadoController.groovy
@@ -9,14 +9,25 @@ class MensajePrivadoController {
     def usuarioService
 
     def index() {
-        render view: "crearMensaje"
+        render view: "enviarMensaje"
     }
 
-    def crearMensaje (String texto, long idUsuarioCreador, long  idUsuarioReceptor, MensajePrivado mensajeAlCualResponde,Archivo archivo) {
-        def usuarioCreador = Usuario.get(idUsuarioCreador)
-        def usuarioReceptor = Usuario.get(idUsuarioReceptor)
+    def enviarMensaje (long idUsuarioCreador, long  idUsuarioReceptor) {
+        def usuarioCreadorInstance = Usuario.get(idUsuarioCreador)
+        def usuarioReceptorInstance = Usuario.get(idUsuarioReceptor)
         MensajePrivado mensaje = mensajePrivadoService.crearMensaje(texto, usuarioCreador, usuarioReceptor, mensajeAlCualResponde, archivo)
         mensajePrivadoService.crearInformacion(usuarioCreador, usuarioReceptor, mensaje)
         usuarioService.enviarMensaje(mensaje,usuarioReceptor)
+    }
+
+    def verMensaje (long idUsuario, long idMensaje, long idInformacion) {
+        def mensajeInstance = MensajePrivado.get(idMensaje)
+        def usuarioInstance = Usuario.get(idUsuario)
+        def infoInstance = InformacionMensajeUsuario.get(idInformacion)
+        if ()
+    }
+
+    def listaMensajes (long idUsuario) {
+
     }
 }
