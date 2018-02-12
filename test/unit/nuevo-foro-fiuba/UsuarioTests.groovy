faebdd79b62a41fa6e765f9c4e23368590469a5b
Germán Rotili <German-Rotili@users.noreply.github.com>
2018-02-12 11:27:50 -0300

    Nuevos Tests

--- test/unit/nuevo-foro-fiuba/UsuarioTests.groovy
+++ test/unit/nuevo-foro-fiuba/UsuarioTests.groovy
@@ -4,11 +4,38 @@ import org.junit.*
 @TestFor(Usuario)
 class UsuarioTests {
 
+    Usuario usuario
+
     void setup() {
-        usuario = new Usuario(nombre: 'Nombre1', apellido: 'Apellido1', nombreUsuario: 'Nick1')
+        usuario = new Usuario(nombre: 'Homero', apellido: 'Simpson', nombreUsuario: 'HSimpson')
     }
 
     void usuarioCreaUnaPublicacionY() {
         fail "Implement me"
     }
+
+    void 'test soy un usuario que quiere comentar en una publicacion pero no tiene el promedido suficiente' () {
+        Publicacion publicacion = new Publicacion(usuarioCreador: usuario, promedioRequeridoParaComentar: 4)
+        Comentario comentario = new Comentario()
+        assert shouldFail(PromedioInsuficienteException) {
+            usuario.comentarPublicacion(comentario, publicacion)
+        }
+    }
+
+    void 'test soy un usuario que quiere comentar en una publicacion cerrada' () {
+        Publicacion publicacion = new Publicacion(usuarioCreador: usuario, promedioRequeridoParaComentar: 4)
+        Comentario comentario = new Comentario()
+        usuario.cambiarEstado(publicacion)
+        assert shouldFail(PublicacionCerradaException) {
+            usuario.comentarPublicacion(comentario, publicacion)
+        }
+    }
+
+    void 'test soy un usuario que crea una publicacion y aparezco como creador' () {
+        Publicacion publicacion = new Publicacion(usuarioCreador: usuario, promedioRequeridoParaComentar: 4)
+        assert shouldFail(PromedioInsuficienteException) {
+            usuario.esDueñoDeLaPublicacion(publicacion)
+        } == true
+    }
+
 }
